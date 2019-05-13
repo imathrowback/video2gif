f6adb36c787bcbb4a3cd6786a2227c7b9e4e587d
imathrowback <imathrowbackg@gar.id.au>
2019-05-13 21:39:04 +1000

    Update video2gif.bat
    Use hardware acceleration for decoding: https://trac.ffmpeg.org/wiki/HWAccelIntro

--- video2gif.bat
+++ video2gif.bat
@@ -92,7 +92,7 @@ SET filters=fps=%fps%,scale=%width%:-1:flags=lanczos
 
 ECHO Generating palette file. Please wait...
 
-CALL ffmpeg -v error %time_range% -i "%input%" -vf "%filters%,palettegen=max_colors=%max_colors%" -y "%palette%"
+CALL ffmpeg -hwaccel dxva2 -v error %time_range% -i "%input%" -vf "%filters%,palettegen=max_colors=%max_colors%" -y "%palette%"
 
 IF NOT EXIST "%palette%" (
 	ECHO Failed to generate palette file.
@@ -101,7 +101,7 @@ IF NOT EXIST "%palette%" (
 
 ECHO Generating gif file. Please wait...
 
-CALL ffmpeg -v error %time_range% -i "%input%" -i "%palette%" -lavfi "%filters% [x]; [x][1:v] paletteuse=dither=%dither%" %overwrite% "%output%"
+CALL ffmpeg -hwaccel dxva2 -v error %time_range% -i "%input%" -i "%palette%" -lavfi "%filters% [x]; [x][1:v] paletteuse=dither=%dither%" %overwrite% "%output%"
 
 IF NOT EXIST "%output%" (
 	ECHO Failed to generate gif file.
